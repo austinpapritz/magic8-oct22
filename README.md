3f4aca3b326b7151fb20fd333b7127f7282a866a
Austin Papritz <austin@papritz.dev>
2022-10-27 15:04:59 -0700

    evenlistener on submit click, README additions, DOM and fortune answers added to JS

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Magic8 README
 
 !(Wireframe)[magic-8_wireframe.png]
+
+Event listen for Submit button
+Write a function to trigger when Submit clicked
--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 /* Imports */
 
 /* Get DOM Elements */
+const submitBtn = document.getElementById('submit-btn');
+const againBtn = document.getElementById('again-btn');
+const promptSection = document.getElementById('prompt-section');
+const answerSection = document.getElementById('answer-section');
+const answerP = document.getElementById('answer-p');
 
 /* State */
 
+const answers = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
+
 /* Events */
+
+submitBtn.addEventListener('click', () => {
+    console.log('submit clicked');
+});
 
 /* Display Functions */
 
